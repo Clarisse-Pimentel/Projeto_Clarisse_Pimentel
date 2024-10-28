391c48d32f39ab4d26792e3c65e5455e97017b0f
unknown <clarilpim@gmail.com>
2024-10-28 18:57:19 -0300

    Adiciona classes

---
+++ A.java
@@ -0,0 +1,34 @@
+public class A {
+    
+    private int A1;
+    private float A2;
+
+    public A(int a1, float a2) {
+        A1 = a1;
+        A2 = a2;
+    }
+
+    public void setA1(int meuA1) {
+        A1 = meuA1;
+    }
+
+    public void setA2(float meuA2) {
+        A2 = meuA2;
+    }
+
+    public int getA1() {
+        return A1;
+    }
+
+    public float A2() {
+        return A2;
+    }
+
+    public void MA1() {
+        System.out.println("Nome do metodo: MA1");
+    }
+
+    public void MA2() {
+        System.out.println("Nome do metodo: MA2");
+    }
+}
---
+++ B.java
@@ -0,0 +1,34 @@
+public class B {
+    
+    private int B1;
+    private float B2;
+
+    public B(int b1, float b2) {
+        B1 = b1;
+        B2 = b2;
+    }
+
+    public void setB1(int meuB1) {
+        B1 = meuB1;
+    }
+
+    public void setB2(float meuB2) {
+        B2 = meuB2;
+    }
+
+    public int getB1() {
+        return B1;
+    }
+
+    public float B2() {
+        return B2;
+    }
+
+    public void MB1() {
+        System.out.println("Nome do metodo: MB1");
+    }
+
+    public void MB2() {
+        System.out.println("Nome do metodo: MB2");
+    }
+}
