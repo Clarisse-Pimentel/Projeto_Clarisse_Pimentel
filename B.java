21b47a99d61e184dd7f19cd2a4744599b1076ced
unknown <clarilpim@gmail.com>
2024-10-28 21:21:56 -0300

    Criados os metodos MB3() e MC3()

--- B.java
+++ B.java
@@ -31,4 +31,8 @@ public class B {
     public void MB2() {
         System.out.println("Nome do metodo: MB2");
     }
+
+    public void MB3() {
+        System.out.println("Nome do metodo: MB3");
+    }
 }
--- C.java
+++ C.java
@@ -32,4 +32,8 @@ public class C {
         System.out.println("Nome do metodo: MC2");
     }
 
+    public void MC3() {
+        System.out.println("Nome do metodo: MC3");
+    }
+
 }
